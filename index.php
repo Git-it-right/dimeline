6d6efb0b8a51a19ad78638235f374594a1b35bf0
Git-it-right <120685065+Git-it-right@users.noreply.github.com>
2024-04-25 17:53:35 +0200

    Add files via upload
    Added First Layer of Directory. Please bear with me I'm trying something new here

--- index.php
+++ index.php
@@ -1,75 +1,220 @@
-<?php       
-        
-echo ' 
+<!doctype html>
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<title>PHP Application</title>
-	<link href="css/site.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.72.0">
+    <title>Dimeline</title>
+
+    <link rel="canonical" href="https://v5.getbootstrap.com/docs/5.0/examples/blog/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <style>
+    /* stylelint-disable selector-list-comma-newline-after */
+
+    .blog-header {
+        line-height: 1;
+        border-bottom: 1px solid #e5e5e5;
+    }
+
+    .blog-header-logo {
+        font-family: "Playfair Display", Georgia, "Times New Roman", serif;
+        font-size: 2.25rem;
+    }
+
+    .blog-header-logo:hover {
+        text-decoration: none;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-family: "Playfair Display", Georgia, "Times New Roman", serif;
+    }
+
+    .display-4 {
+        font-size: 2.5rem;
+    }
+
+    @media (min-width: 768px) {
+        .display-4 {
+            font-size: 3rem;
+        }
+    }
+
+    .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+    }
+
+    .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+    }
+
+    .nav-scroller .nav-link {
+        padding-top: .75rem;
+        padding-bottom: .75rem;
+        font-size: .875rem;
+    }
+
+    .card-img-right {
+        height: 100%;
+        border-radius: 0 3px 3px 0;
+    }
+
+    .flex-auto {
+        flex: 0 0 auto;
+    }
+
+    .h-250 {
+        height: 250px;
+    }
+
+    @media (min-width: 768px) {
+        .h-md-250 {
+            height: 250px;
+        }
+    }
+
+    /* Pagination */
+    .blog-pagination {
+        margin-bottom: 4rem;
+    }
+
+    .blog-pagination>.btn {
+        border-radius: 2rem;
+    }
+
+    /*
+        * Blog posts
+        */
+    .blog-post {
+        margin-bottom: 4rem;
+    }
+
+    .blog-post-title {
+        margin-bottom: .25rem;
+        font-size: 2.5rem;
+    }
+
+    .blog-post-meta {
+        margin-bottom: 1.25rem;
+        color: #727272;
+    }
+
+    /*
+        * Footer
+        */
+    .blog-footer {
+        padding: 2.5rem 0;
+        color: #727272;
+        text-align: center;
+        background-color: #f9f9f9;
+        border-top: .05rem solid #e5e5e5;
+    }
+
+    .blog-footer p:last-child {
+        margin-bottom: 0;
+    }
+    </style>
 </head>
 
 <body>
-<![endif]-->
 
-<div class="main-container">
-<iframe width="1080px" height="1920px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__tJO_StURVVPNVFGRTZENkpUVkMzSVpVRFUxNURXQi4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh"
-    allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+    <div class="container">
+        <header class="blog-header py-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-4 pt-1">
 
-        <div class="cloud-image">
-            <img src="img/successCloudNew.svg" alt="successCloudNew" />
-        </div>
-        <div class="content">
-            <div class="tweet-container">
-            <a href="http://twitter.com/intent/tweet/?text=I%20just%20created%20a%20new%20PHP%20website%20on%20Azure%20using%20Github%20Actions&hashtags=%40GithubActions%20%40Azure">
-                <img src="img/tweetThis.svg" alt="tweetThis" />
-            </a>
-        </div>
-            <div class="content-body">
-                <div class="success-text">CentralPark</div>
-                <div class="description line-1">
-			
-				"Your Github Repository: CENTRALPARK with Github Actions has been successfully setup"
-				
-				</div>
-                <div class="description line-2">
-				<?php
-				$appType = "PHP";
-				echo "Your $appType app is up and running on Azure";
-				?>
-				</div>
-                <div class="next-steps-container">
-                    <div class="next-steps-header">Next up</div>
-                    <div class="next-steps-body">
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/cloneWhite.svg" alt="cloneWhite" >
-                            </div>
-                            <div class="step-text"><a href="https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository">Clone your code repository and start developing your application on IDE of your choice</a></div>
-                        </div>
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/deployWhite.svg" alt="deployWhite">
-                            </div>
-                            <div class="step-text"><a href="https://docs.github.com/en/actions">View your CI/CD pipeline on Github and customize it as per your needs</a></div>
-                        </div>
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/stackWhite.svg" alt="stackWhite">
-                            </div>
-                            <div class="step-text"><a href="http://portal.azure.com">View your service stack in the Azure Portal</a></div>
-                        </div>
-                        <div class="step">
-                            <div class="step-icon">
-                                <img src="img/lightbulbWhite.svg" alt="lightbulbWhite">
-                            </div>
-                            <div class="step-text"><a href="https://docs.github.com">Learn more about all you can do with Github by visiting the documentation</a></div>
-                        </div>
-                    </div>
+                </div>
+                <div class="col-4 text-center">
+                    <a class="blog-header-logo text-dark" href="#">Dimeline</a>
+                </div>
+                <div class="col-4 d-flex justify-content-end align-items-center">
+                    <a class="link-secondary" href="#" aria-label="Search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img"
+                            viewBox="0 0 24 24">
+                            <title>Search</title>
+                            <circle cx="10.5" cy="10.5" r="7.5" />
+                            <path d="M21 21l-5.2-5.2" />
+                        </svg>
+                    </a>
+
                 </div>
             </div>
+        </header>
+
+        <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-between">
+                <a class="p-2 link-secondary" href="./ui/people.php">People</a>
+                <a class="p-2 link-secondary" href="./GIT god/git.php">GIT</a>
+                <a class="p-2 link-secondary" href="./Azure GOD/azure_cert.php">Azure Certifications</a>
+                <a class="p-2 link-secondary" href="./skeleton.php">Skeleton</a>
+            </nav>
         </div>
-    </div>
+
+
+
+        
+
+    <footer class="blog-footer">
+        <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a
+                href="https://twitter.com/mdo">@mdo</a>.</p>
+        <p>
+            <a href="#">Back to top</a>
+        </p>
+    </footer>
+
+
+
 </body>
-</html>';
-?>
+
+</html>
